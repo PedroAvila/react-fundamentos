7588d0ce6a5eb93c047170d7a31f5b7c243e752d
PedroAvila <minimainor_austin@hotmail.com>
2021-05-15 06:00:23 -0500

    Expresiones en JSX

--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import React from 'react'
 import reactDom from 'react-dom';
 import ReactDOM from 'react-dom'
 
-const app = <h1>Hola React ****</h1>
+const name = "Gerardo Gallegos"
+
+const user1 = {
+  name: "Gerardo Gallegos",
+  age: 28,
+  country: "México"
+}
+
+function getInfo(user){
+  return 'Hola mi nombre es ${ user.name } y soy originario de México'
+}
+
+// const app = <h1>Tengo { user1.age } años y el doble de mi edad es { user1.age * 2 } </h1>
+const app = <h1> { getInfo(user1) } </h1>
 
 // const root = ;
 
