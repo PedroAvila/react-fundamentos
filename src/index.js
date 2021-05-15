7863e81220d5dc341564355370c5ae15f63499f4
PedroAvila <minimainor_austin@hotmail.com>
2021-05-15 06:24:46 -0500

    Crear componente funcional con React

--- src/index.js
+++ src/index.js
@@ -2,21 +2,24 @@ import React from 'react'
 import reactDom from 'react-dom';
 import ReactDOM from 'react-dom'
 
-const name = "Gerardo Gallegos"
+const name = "React"
 
-const user1 = {
-  name: "Gerardo Gallegos",
-  age: 28,
-  country: "México"
-}
+/* Los componentes siempre tienen que comenzar con mayúscula
+Y deben retornar un nodo */
+const TarjetaFruta = () => (
+  //return <div>Hey!</div>
+  <div>
+      <h3>Titulo</h3>
+      <hr />
+      <p>Descripción ....</p>
+    </div>
+)
 
-function getInfo(user){
-  return 'Hola mi nombre es ${ user.name } y soy originario de México'
-}
+const App = () => (
+  <div>
+    <TarjetaFruta />
+    <TarjetaFruta></TarjetaFruta>
+  </div>
+)
 
-// const app = <h1>Tengo { user1.age } años y el doble de mi edad es { user1.age * 2 } </h1>
-const app = <h1> { getInfo(user1) } </h1>
-
-// const root = ;
-
-reactDom.render(app, document.getElementById("root"));
+reactDom.render(<App />, document.getElementById("root"));
